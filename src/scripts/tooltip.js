b36cae69a48cdf4edb797e40015f43f37d9f2c6a
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2024-10-10 19:37:44 -0500

    make font system & font-awesome logos local instead of cdn hosted

---
+++ src/scripts/tooltip.js
@@ -0,0 +1,37 @@
+(function () {
+    const tooltip = document.getElementById("tooltip");
+  
+    document.addEventListener("mouseover", function (event) {
+      const target = event.target;
+      if (target.tagName.toLowerCase() === "a") {
+        const href = target.getAttribute("href");
+  
+        const link = document.createElement("a");
+        link.href = href || "";
+  
+        if (link.hostname && link.hostname !== location.hostname) {
+          const displayHostname = link.hostname.replace(/^www\./, "");
+  
+          tooltip.textContent = displayHostname;
+          tooltip.style.display = "block";
+          tooltip.style.left = event.pageX + 10 + "px";
+          tooltip.style.top = event.pageY + 10 + "px";
+        }
+      }
+    });
+  
+    document.addEventListener("mouseout", function (event) {
+      const target = event.target;
+      if (target.tagName.toLowerCase() === "a") {
+        tooltip.style.display = "none";
+      }
+    });
+  
+    document.addEventListener("mousemove", function (event) {
+      if (tooltip.style.display === "block") {
+        tooltip.style.left = event.pageX + 3 + "px";
+        tooltip.style.top = event.pageY - 25 + "px";
+      }
+    });
+  })();
+  
