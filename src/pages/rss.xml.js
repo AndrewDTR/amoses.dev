bca042c27aef862508cd3225ff9ef905ac567dd3
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2024-10-14 15:57:00 -0500

    add rss & button to footer

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,12 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function GET(context) {
+    return rss({
+        title: 'Andrew Moses',
+        description: 'The personal website & blog of Andrew Moses, a Computer Science student at the University of Wisconsin-Madison.',
+        site: context.site,
+        items: await pagesGlobToRssItems(
+            import.meta.glob('./blog/*.{md,mdx}'),
+        ),
+    });
+}
