72df0774399846eab806ebff5910abd616b62754
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2025-02-09 23:00:06 -0600

    update index (3->5) & change rss author name

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,13 +1,21 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
+    const originalItems = await pagesGlobToRssItems(
+        import.meta.glob('./blog/*.{md,mdx}')
+    );
+
+    const itemsWithAuthor = originalItems.map((item) => ({
+        ...item,
+        author: 'Andrew Moses',
+    }));
+
     return rss({
         title: 'Andrew Moses',
-        description: 'The personal website & blog of Andrew Moses, a Computer Science student at the University of Wisconsin-Madison.',
+        description:
+            'The personal website & blog of Andrew Moses, a Computer Science student at the University of Wisconsin-Madison.',
         site: context.site,
-        items: await pagesGlobToRssItems(
-            import.meta.glob('./blog/*.{md,mdx}'),
-        ),
+        items: itemsWithAuthor,
         stylesheet: '/rss/styles.xsl',
     });
 }
